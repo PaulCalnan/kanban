20ee27bc0cdfe171fad31f898b287fd61b0a74ff
Paul Calnan <pcalnan71@gmail.com>
2022-01-08 08:26:40 +1100

    implement item drag drop feature

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const saveItemBtns = document.querySelectorAll('.solid');
 const addItemContainers = document.querySelectorAll('.add-container');
 const addItems = document.querySelectorAll('.add-item');
 // Item Lists
-const itemLists = document.querySelectorAll('.drag-item-list');
+const listColumns = document.querySelectorAll('.drag-item-list');
 const backlogList = document.getElementById('backlog-list');
 const progressList = document.getElementById('progress-list');
 const completeList = document.getElementById('complete-list');
@@ -19,8 +19,9 @@ let completeListArray = [];
 let onHoldListArray = [];
 let listArrays = [];
 
-// Drag Functionality
+// Drag Functionality initialise global vars
 let draggedItem;
+let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
 function getSavedColumns() {
@@ -56,7 +57,7 @@ function createItemEl(columnEl, column, item, index) {
   const listEl = document.createElement('li');
   listEl.classList.add('drag-item');
   listEl.textContent = item;
-  listEl.draggable = true;
+  listEl.draggable = true; // Allows item to grab and drag instead of default ie highlight text
   listEl.setAttribute('ondragstart', 'drag(event)');
   // Append
   columnEl.appendChild(listEl);
@@ -88,18 +89,39 @@ function updateDOM() {
   onHoldListArray.forEach((onHoldItem, index) => {
     createItemEl(onHoldList, 0, onHoldItem, index);
   });
-  // Run getSavedColumns only once, Update Local Storage
+  // Run getSavedColumns only once, update local storage
 
 
 }
 
-// When Item Starts Dragging
+// When item starts dragging
 function drag(e) {
   draggedItem = e.target;
   console.log('draggedItem:', draggedItem);
 }
 
 // Column allows for item to drop
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+// When item enters new column area
+function dragEnter(column) {
+  listColumns[column].classList.add('over');
+  currentColumn = column;
+}
+
+// Dropping items into new column
+function drop(e) {
+  e.preventDefault();
+  // On Drop, remove column background colour and padding added from above
+  listColumns.forEach((column) => {
+    column.classList.remove('over');
+  });
+  // Add item into new column
+  const parent = listColumns[currentColumn];
+  parent.appendChild(draggedItem);
+}
 
 // On Load
 updateDOM();
